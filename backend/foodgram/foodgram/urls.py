36283e140ad5a3bce2fc9bbbb413780b595ab896
Мария Баранова <baranova.m.p@mail.ru>
2023-09-03 19:48:13 +0500

    version_1

---
+++ backend/foodgram/foodgram/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView, TokenVerifyView)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('api/v1/jwt/create/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('api/v1/jwt/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
+    path('api/v1/jwt/verify/', TokenVerifyView.as_view(),
+         name='token_verify'),
+]
+
+handler404 = 'core.views.page_not_found' # хандлеры добавить в приложение кор
+handler403 = 'core.views.permission_denied'
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
