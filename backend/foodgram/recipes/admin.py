3831a8bd7c86fdf8b00230d00867665e72e1dc78
Мария Баранова <baranova.m.p@mail.ru>
2023-09-05 16:50:31 +0500

    до настройки выборов ингр в админке

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Recipe, Tag, Ingredient, Favorite, ShoppingCart, Subscribe
 
-# Register your models here.
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    list_filter = ('name',)
+
+
+class RecipesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'favorite_count')
+    list_filter = ('name', 'author', 'tag',)
+
+    def favorite_count(self, obj):
+        return obj.favorites.all().count()
+
+
+admin.site.register(Tag)
+admin.site.register(Ingredient)
+admin.site.register(Recipe)
+admin.site.register(Favorite)
+admin.site.register(Subscribe)
+admin.site.register(ShoppingCart)
--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -4,6 +4,26 @@ from django.db import models
 User = get_user_model()
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=60, help_text="Название тэга")
+    color = models.CharField(max_length=7,
+                             help_text="Цвет тэга (например, '#FF0000')")
+    slug = models.CharField(max_length=60,
+                            help_text="Уникальный идентификатор тэга")
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=60)
+    amount = models.IntegerField(null=True)
+    measurement_unit = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'{self.name} ({self.measurement_unit})'
+
+
 class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='recipes')
@@ -22,3 +42,43 @@ class Recipe(models.Model):
         return self.name
 
 
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE)
+    recipes = models.ManyToManyField(Recipe, related_name='favorites')
+
+
+class Subscribe(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower')
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following')
+
+    def __str__(self):
+        return str(self.user)
+
+
+class ShoppingListIngredient(models.Model):
+    shopping_list = models.ForeignKey(
+        'ShoppingCart',
+        on_delete=models.CASCADE,
+        related_name='ingredients_for_buy'
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name='shopping_lists'
+    )
+    amount = models.IntegerField()
+
+
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE)
+    ingredients = models.ManyToManyField(
+        ShoppingListIngredient,
+        related_name='shopping_carts')
