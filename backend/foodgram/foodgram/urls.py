c1c4ce42f832ac3a0578673d0b5b98863a100ad7
Мария Баранова <baranova.m.p@mail.ru>
2023-09-30 15:51:18 +0500

    flake8

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -6,12 +6,12 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
 
 
 urlpatterns = [
-     path('admin/', admin.site.urls),
-     path('api/', include('api.urls')),
-     path('api/auth/token/login/', TokenObtainPairView.as_view(),
-          name='token_obtain_pair'),
-     path('api/auth/token/refresh/', TokenRefreshView.as_view(),
-          name='token_refresh'),
-     path('api/auth/token/verify/', TokenVerifyView.as_view(),
-          name='token_verify'),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('api/auth/token/login/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
+    path('api/auth/token/verify/', TokenVerifyView.as_view(),
+         name='token_verify'),
 ]
